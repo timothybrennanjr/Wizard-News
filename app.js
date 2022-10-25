2adf25f047e2273d90d47f3d60125481a7fab91a
timothy <timothybrennan8@gmail.com>
2022-10-25 10:14:49 -0400

    Moved the postBank folder into our directory and now we can display some post information in the webpage.

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const morgan = require("morgan");
+const postBank = require("./postBank");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello everybody!"));
 app.use(morgan("dev"));
 
-const PORT =2000;
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  <h1>Wizard News</h1>
+  </head>
+  <body>
+  <ul>
+  ${posts.map((post) => `<li>${post.name}</li>`)}
+  </ul>
+  </body>
+  </html>`;
+
+  res.send(html);
+});
+
+const PORT = 2000;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
