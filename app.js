f1d3b093e8193a54562dcfa909f56176b4f74e1b
Brianna <yaniilovegirl0023@gmail.com>
2022-10-25 10:11:48 -0500

    added single-post route

--- app.js
+++ app.js
@@ -35,6 +35,27 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
+app.get( '/posts/:id', (req, res) => {
+const id = req.params.id;
+const post = postBank.find(id);
+const html = `<!DOCTYPE html>
+<html>
+<head>
+  <title>Wizard News</title>
+  <link rel="stylesheet" href="/style.css" />
+</head>
+<body>
+  <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+    
+    </div>
+  </body>
+</html>`
+
+res.send(html);});
+
+
+
 const PORT = 2000;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
